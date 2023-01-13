0542d1047a1013465aa0dc85035f695ed488fd08
Zul Faza Makarima <zulfazamakarima@gmail.com>
2023-01-14 00:04:37 +0700

    add footer

---
+++ src/lib/components/Footer/ButtonWithPopup.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { IconType } from './Footer.svelte';
+	import { fade, fly } from 'svelte/transition';
+	export let icon: IconType;
+	let popup = false;
+	const handleClick = () => {
+		popup = true;
+		setTimeout(() => {
+			popup = false;
+		}, 800);
+		if (icon.click) icon.click();
+	};
+</script>
+
+<button on:click={handleClick} class="relative">
+	{#if popup}
+		<div out:fade in:fly class=" absolute -top-5 -right-16 text-sm bg-[#4C88B4] rounded-full px-4">
+			copied
+		</div>
+	{/if}
+	<svelte:component this={icon.icon} />
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { browser } from '$app/environment';
 	import Navbar from '$lib/Navbar.svelte';
 	import { darkMode } from '$lib/store/dark';
+	import Footer from '$lib/components/Footer/Footer.svelte';
 	const loadDarkModeFromLocalStorage = () => {
 		if (!browser) return;
 		if (
@@ -30,4 +31,5 @@
 	<main class="w-full">
 		<slot />
 	</main>
+	<Footer />
 </div>
