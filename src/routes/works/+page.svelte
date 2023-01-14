45c661936543cd8651261ce493298627f12f82d3
Zul Faza Makarima <zulfazamakarima@gmail.com>
2023-01-14 20:06:37 +0700

    setup my works page

---
+++ src/routes/works/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import MyWorks from '$lib/assets/icons/MyWorks.svelte';
+</script>
+
+<svelte:head>
+	<title>#My Works</title>
+</svelte:head>
+
+<header class="flex-cc w-full pt-36 pb-20">
+	<div class=" max-w-md w-full h-full">
+		<MyWorks />
+	</div>
+</header>
