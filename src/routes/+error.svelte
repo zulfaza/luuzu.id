f83d1cdc817b05d0960e35d63d221b8378737de2
Zul Faza Makarima <zulfazamakarima@gmail.com>
2023-01-15 18:39:20 +0700

    update error page min height

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 </script>
 
@@ -6,7 +6,7 @@
 	<title>Error 404</title>
 </svelte:head>
 
-<div class="w-full min-h-screen flex-cc col">
+<div class="w-full h-full flex-cc col min-h-[calc(100vh-120px)] text-dark dark:text-white">
 	<h1 class="text-[128px] leading-[152px] font-extrabold">404</h1>
 	<h2 class="font-bold text-xl">Error Not Found</h2>
 	<div class="flex-cc mt-4">
